96aa15659f5b1027d856dafbe91a69dfafa562cf
Pravanyavethsa <pravanya.vethsa@gmail.com>
2023-07-10 13:49:20 -0700

    added Test file

--- src/main/java/com/cabinvoice/CabInvoiceGenerator.java
+++ src/main/java/com/cabinvoice/CabInvoiceGenerator.java
@@ -5,7 +5,7 @@ public class CabInvoiceGenerator {
  public static final double costpermin=1.0;
  public static final double min_fare=5;
 
- public static double calculatefare(double distance,int time){
+ public static double calculateFare(double distance,int time){
      double totalValue=(distance*costperkm)+(time*costpermin);
      if (totalValue<5){
       return min_fare;
--- src/test/java/com/cabtest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabtest/CabInvoiceGeneratorTest.java
@@ -1,9 +1,18 @@
 package com.cabtest;
 
 
+import com.cabinvoice.CabInvoiceGenerator;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CabInvoiceGeneratorTest {
-
-
+    CabInvoiceGenerator cabInvoiceGenerator=new CabInvoiceGenerator();
+    @Test
+    public void TotalFare(){
+        double distance=2.0;
+        int time=0;
+        double result=cabInvoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(25.0,result,5);
+    }
 
 }
