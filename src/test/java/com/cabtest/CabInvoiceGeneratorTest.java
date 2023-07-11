c07c35a82d71ee5f6d1463b9c14a39691a80d32e
Pravanyavethsa <pravanya.vethsa@gmail.com>
2023-07-11 14:01:25 -0700

    added:Test file

--- src/test/java/com/cabtest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabtest/CabInvoiceGeneratorTest.java
@@ -2,6 +2,7 @@ package com.cabtest;
 
 import com.cabinvoice.Invoice;
 import com.cabinvoice.Ride;
+import com.cabinvoice.RideRepository;
 import junit.framework.TestCase;
 import com.cabinvoice.CabInvoiceGenerator;
 import org.junit.Assert;
@@ -42,5 +43,29 @@ public class CabInvoiceGeneratorTest {
         Invoice actualInvoice = cabInvoice.getInvoiceOfRidesOfBothRides(rides);
         Invoice expectedInvoice = new Invoice(3, 603, 201);
         Assertions.assertEquals(expectedInvoice, actualInvoice);
+    }
+    @Test
+    public void givenMultipleRidesShould_ReturnInvoice() {
+        Invoice invoice = new Invoice();
+        Ride[] rides = {new Ride(12.0, 3.0), new Ride(14.0, 5.0), new Ride(0.1, 3.0)};
+        Invoice actualInvoice = cabInvoiceGenerator.getInvoiceOfRides(rides);
+        Invoice expectedInvoice = new Invoice(3, 273, 91);
+        Assertions.assertEquals(expectedInvoice, actualInvoice);
+    }
+
+        @Test
+    public void givenUserId_OfMultipleRidesShould_ReturnInvoice(){
+        Ride[] rides1 = {new Ride(12.0, 3.0), new Ride(14.0, 5.0), new Ride(0.1, 3.0)};
+        Ride[] rides2 = {new Ride(12.5, 4.8), new Ride(34.4, 9.7)};
+        RideRepository repository = new RideRepository();
+        repository.addRides("user101", rides1);
+        repository.addRides("user102", rides2);
+        Invoice actualInvoice1 = repository.getInvoiceFromUserId("user102");
+        Invoice actualInvoice2 = repository.getInvoiceFromUserId("user101");
+        Invoice expectedInvoice1 = new Invoice(2, 483.5, 241.75);
+        Invoice expectedInvoice2 = new Invoice(3, 273, 91);
+        Assertions.assertEquals(expectedInvoice1, actualInvoice1);
+        Assertions.assertEquals(expectedInvoice2, actualInvoice2);
+
     }
 }
